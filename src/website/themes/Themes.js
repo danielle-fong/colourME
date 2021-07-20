6f3815ffb422fc40a73b840a5fb3fb1194898d73
danielle-fong <daniellefong13@gmail.com>
2021-07-20 14:45:09 -0400

    commit on themes

--- src/website/themes/Themes.js
+++ src/website/themes/Themes.js
@@ -3,10 +3,18 @@ import "./Themes.css";
 function Themes(){
 	return (
 		<>
-			<div className = "block">
-            <h3 className= "elegant">ELEGANT</h3>
-            <h4 className = "minimal">MINIMAL</h4>
-            <h5 className = "modern">MODERN</h5>
+			<div className = "top">
+			<h1 className = "elegant">ELEGANT</h1>
+			<h1 className = "minimal">MINIMAL</h1>
+			<h1 className = "modern">MODERN</h1>
+			<h1 className = "nature">NATURE</h1>
+			</div>
+
+			<div className = "bottom">
+			<h1 className = "neon">NEON</h1>
+			<h1 className = "pastel">PASTEL</h1>
+			<h1 className = "tropical">TROPICAL</h1>
+			<h1 className = "vintage">VINTAGE</h1>
             </div>
 		</>
 	);
