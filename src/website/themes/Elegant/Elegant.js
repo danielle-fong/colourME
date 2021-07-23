6fef129e6c7d3777ea9418fbb7ce221a71d362ef
danielle-fong <daniellefong13@gmail.com>
2021-07-23 13:15:20 -0400

    commit on theme shades

--- src/website/themes/Elegant/Elegant.js
+++ src/website/themes/Elegant/Elegant.js
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 function Elegant(){
 	return (
 		<>
-			<div className = "header">Choose your shade</div>
+			<div className = "header">Choose your shade:</div>
 
 			<div className = "block">
-			<h1 className = "light">LIGHT</h1>
-			<h1 className = "dark">DARK</h1>
+			<Link className="light" to="/Light">
+			<h1>LIGHT</h1>
+			</Link>
+			<Link className="dark" to="/Dark">
+			<h1>DARK</h1>
+			</Link>
 			</div>
 		</>
 	);
