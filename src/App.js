40d55e13d82aa46f8a71e06aca0f9ca9a45cdfef
danielle-fong <daniellefong13@gmail.com>
2021-07-25 15:45:49 -0400

    commit on app

--- src/App.js
+++ src/App.js
@@ -21,6 +21,18 @@ import Neon from "./website/themes/Neon/Neon";
 import Neon_Dark from "./website/themes/Neon/Dark/Neon_Dark";
 import Neon_Light from "./website/themes/Neon/Light/Neon_Light";
 
+import Pastel from "./website/themes/Pastel/Pastel";
+import Pastel_Dark from "./website/themes/Pastel/Dark/Pastel_Dark";
+import Pastel_Light from "./website/themes/Pastel/Light/Pastel_Light";
+
+import Tropical from "./website/themes/Tropical/Tropical";
+import Tropical_Dark from "./website/themes/Tropical/Dark/Tropical_Dark";
+import Tropical_Light from "./website/themes/Tropical/Light/Tropical_Light";
+
+import Vintage from "./website/themes/Vintage/Vintage";
+import Vintage_Dark from "./website/themes/Vintage/Dark/Vintage_Dark";
+import Vintage_Light from "./website/themes/Vintage/Light/Vintage_Light";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -50,7 +62,19 @@ const App = () => {
 					<Route path="/Neon" exact component={Neon}/>
 					<Route path="/Neon_Dark" exact component={Neon_Dark}/>
 					<Route path="/Neon_Light" exact component={Neon_Light}/>
+
+					<Route path="/Pastel" exact component={Pastel}/>
+					<Route path="/Pastel_Dark" exact component={Pastel_Dark}/>
+					<Route path="/Pastel_Light" exact component={Pastel_Light}/>
+
+					<Route path="/Tropical" exact component={Tropical}/>
+					<Route path="/Tropical_Dark" exact component={Tropical_Dark}/>
+					<Route path="/Tropical_Light" exact component={Tropical_Light}/>
 					
+					<Route path="/Vintage" exact component={Vintage}/>
+					<Route path="/Vintage_Dark" exact component={Vintage_Dark}/>
+					<Route path="/Vintage_Light" exact component={Vintage_Light}/>
+
 				</Switch>
 			</Router>
 		</>
---
+++ src/website/themes/Tropical/Tropical.js
@@ -0,0 +1,21 @@
+import "./Tropical.css";
+import { Link } from "react-router-dom";
+
+function Pastel(){
+	return (
+		<>
+			<div className = "header">Choose your shade:</div>
+
+			<div className = "block">
+			<Link className="light" to="/Light">
+			<h1>LIGHT</h1>
+			</Link>
+			<Link className="dark" to="/Dark">
+			<h1>DARK</h1>
+			</Link>
+			</div>
+		</>
+	);
+};
+
+export default Pastel;
