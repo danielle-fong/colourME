e5f9c90dc711913fe241a8f729abdf50a253aaa8
danielle-fong <daniellefong13@gmail.com>
2021-07-27 17:53:17 -0400

    commit on themes

--- src/website/themes/Tropical/Tropical.js
+++ src/website/themes/Tropical/Tropical.js
@@ -1,21 +1,27 @@
 import "./Tropical.css";
 import { Link } from "react-router-dom";
+import background from "./background.png";
 
-function Pastel(){
+function Tropical(){
 	return (
 		<>
+			<div className = "page">
 			<div className = "header">Choose your shade</div>
 
 			<div className = "block">
+			
 			<Link className="light" to="/Light">
 			<h1>LIGHT</h1>
 			</Link>
+
 			<Link className="dark" to="/Dark">
 			<h1>DARK</h1>
 			</Link>
+			</div>
+			<div className="col"><img className="background" src={background}/></div>
 			</div>
 		</>
 	);
 };
 
-export default Pastel;
+export default Tropical;
